793d3d32bb155dcb43722f49aec5a51ea69d5364
Mahmoud Al-Didamoni <mahmoudaldidamoni@gmail.com>
2026-03-15 14:40:22 +0200

    Add common logging toolkit with Kermit logger implementation

--- app/src/main/java/com/didamoni/persona_im/PersonaIMApp.kt
+++ app/src/main/java/com/didamoni/persona_im/PersonaIMApp.kt
@@ -1,6 +1,8 @@
 package com.didamoni.persona_im
 
 import android.app.Application
+import com.didamoni.persona_im.common.logging.Log
+import com.didamoni.persona_im.common.logging.kermit.KermitLogger
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.annotation.KoinApplication
@@ -11,6 +13,8 @@ class PersonaIMApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Log.setLogger(KermitLogger)
 
         startKoin<PersonaIMApp> {
             androidContext(this@PersonaIMApp)
